aa467fd86f4be8a4a44d410da27f25e11e70b530
Guilherme Boiaski <gb006425@cfjl.com.br>
2022-11-09 23:15:47 -0300

    Criado método d carga d dados do banco p/ a classe

--- public/Index.php
+++ public/Index.php
@@ -7,4 +7,8 @@ use Petshop\Model\Produto;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dica = new Dica();
-$dica->find([]);
+$dica->loadById(1);
+echo $dica->iddica . PHP_EOL;
+echo $dica->ttulo . PHP_EOL;
+echo $dica->descricao . PHP_EOL;
+echo $dica->created_at . PHP_EOL;
--- src/Core/DB.php
+++ src/Core/DB.php
@@ -48,6 +48,8 @@ class DB
                 error_log('Erro ao preparar a consulta: ' . $sql);
                 throw new Exception ('Falha a preparar comando SQL');
             }
+
+            $params = array_values($params);
             if (!$st->execute($params)) {
                 error_log('Erro ao executar comando SQL: ' . $sql . '-' .
                 var_export($params, true));
@@ -77,6 +79,8 @@ class DB
                 error_log('Erro ao preparar a consulta: ' . $sql);
                 throw new Exception ('Falha a preparar comando SQL');
             }
+
+            $params = array_values($params);
             if (!$st->execute($params)) {
                 error_log('Erro ao executar comando SQL: ' . $sql . '-' .
                 var_export($params, true));
