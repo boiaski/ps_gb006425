42f58ce7a416774d350ec4f4a91e675aa569bb70
Guilherme Boiaski <gb006425@cfjl.com.br>
2022-11-22 00:07:26 -0300

    Criada estrutura para cadastro de cliente

--- src/Controller/CadastroController.php
+++ src/Controller/CadastroController.php
@@ -20,6 +20,25 @@ class CadastroController extends FrontController
 
     private function formCadastro()
     {
-        
+        $dados = [
+            'btn_label'=>'Criar linha conta',
+            'btn_class'=>'btn btn-success mt-4',
+            'fields'=>[
+                ['type'=>'radio-inline', 'class'=>'col-6', 'label'=>'Você é pessoa...', 'name'=>'tipo',
+                    'options'=>[
+                        ['label'=>'Física', 'value'=>'F'],
+                        ['label'=>'Jurídica', 'value'=>'J'],
+                    ],
+                    'required'=>true,
+                ],
+                ['type'=>'text', 'class'=>'col-6', 'label'=>'Documento', 'name'=>'cpfcnpj', 'required'=>true],
+                ['type'=>'text', 'name'=>'nome', 'label'=>'Seu nome completo', 'required'=>true],
+                ['type'=>'email', 'name'=>'email', 'label'=>'Seu e-mail', 'required'=>true],
+                ['type'=>'password', 'class'=>'col-6', 'name'=>'senha', 'label'=>'Crie uma senha', 'required'=>true],
+                ['type'=>'password', 'class'=>'col-6', 'name'=>'senha2', 'label'=>'Confirme a senha', 'required'=>true],
+            ]
+        ];
+
+        return Render::block('form', $dados);
     }
 }
