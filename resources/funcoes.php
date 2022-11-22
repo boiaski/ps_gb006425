7f70a6f7283997ca1cf8ee4ea5ad6e8a344bb0c1
Guilherme Boiaski <gb006425@cfjl.com.br>
2022-11-22 20:45:11 -0300

    Criado arquivo de funções disponiveis globalmente

---
+++ resources/funcoes.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Função que retorna um ALERT da mensagem presa na sessão e elimina o seu conteúdo
+ *
+ * @return void
+ */
+function retornaHTMLAlertMensagemSessao()
+{
+    if(!isset($_SESSION['mensagem']) || !is_array($_SESSION['mensagem'])) {
+        return '';
+    }
+
+    $tipo = $_SESSION['mensagem']['tipo'];
+    $texto = $_SESSION['mensagem']['texto'];
+
+    $bootstrapAlert = <<<HTML
+        <div class="alert alert-{$tipo}" role="alert">
+            {$texto}
+        </div>
+    HTML;
+
+    unset($_SESSION['mensagem']);
+
+    return $bootstrapAlert;
+}
