26217caefd80b03740e783b2f2f0af5757119cc5
Guilherme Boiaski <gb006425@cfjl.com.br>
2022-11-29 00:07:03 -0300

    Criada estrutura para manutenção de clientes

---
+++ src/Controller/AdminClienteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Petshop\Controller;
+
+use Petshop\View\Render;
+
+class AdminClienteController
+{
+    public function listar()
+    {
+        $dados = [];
+        $dados['titulo'] = 'Clientes';
+
+        Render::back('clientes', $dados);
+    }
+}
